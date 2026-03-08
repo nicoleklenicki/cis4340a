3de4f359415f0dd1e45e248deecb219b28dccf26
nicoleklenicki <nicoleklenicki@gmail.com>
2026-03-08 21:43:36 +0000

    Fix DCL00-J by preventing class initialization cycle

--- selected/R01_DCL00_J.java
+++ selected/R01_DCL00_J.java
@@ -1,16 +1,21 @@
 //Rule: DCL00
+//Compliant Version
 
 public class R01_DCL00_J {
     static class A {
-        static int value = B.value + 1;
+        static int getValue() {
+            return 1;
+        }
     }
 
     static class B {
-        static int value = A.value + 1;
+        static int getValue() {
+            return A.getValue() + 1;
+        }
     }
 
     public static void main(String[] args) {
-        System.out.println(A.value);
-        System.out.println(B.value);
+        System.out.println(A.getValue());
+        System.out.println(B.getValue());
     }
 }
