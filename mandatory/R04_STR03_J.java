c03dae428df5c06ed7b51e7c3d08045cc606f2e9
nicoleklenicki <nicoleklenicki@gmail.com>
2026-03-08 21:38:05 +0000

    Fix STR03-J by avoiding encoding binary data as string

--- mandatory/R04_STR03_J.java
+++ mandatory/R04_STR03_J.java
@@ -1,14 +1,15 @@
 //Rule: STR03
+//Compliant Version
 
 import java.math.BigInteger;
 
 public class R04_STR03_J {
     public static void main(String[] args) {
         BigInteger x = new BigInteger("530500452766");
-        byte[] byteArray = x.toByteArray();
-        String s = new String(byteArray);
-        byteArray = s.getBytes();
-        x = new BigInteger(byteArray);
+        String s = x.toString();
+        byte[] byteArray = s.getBytes();
+        String ns = new String(byteArray);
+        x = new BigInteger(ns);
 
         System.out.println(x);
     }
